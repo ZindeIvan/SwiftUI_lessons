6ace93dbd62c717ed97584b1df7e01e8500d9dc4
ZindeIvan <zinde.ivan@yandex.ru>
2020-07-03 19:28:41 +0600

    init commit

---
+++ VK_client/VK_client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK_client
+//
+//  Created by Зинде Иван on 7/3/20.
+//  Copyright © 2020 zindeivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
